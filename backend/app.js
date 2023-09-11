3f6fc80b50a37fe4287c64016ca352bcb804bbac
stevewithnojobs <swayamkumar409@gmail.com>
2023-09-12 00:48:50 +0530

    f

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,13 @@ const cookieParser=require("cookie-parser")
 const order=require("./routes/orderRoute")
 const bodyParser=require("body-parser")
 const fileUpload=require("express-fileupload")
+const cors=require("cors")
 
 app.use(cookieParser())
 app.use(express.json({limit:'100mb'}))
 app.use(bodyParser.urlencoded({extended:true,limit:'100mb'}))
 app.use(fileUpload())
-
+app.use(cors())
 
 app.use(order)
 app.use(product)
