880c15bd3257a701e6f7a12134ef6c606ab17ceb
stevewithnojobs <swayamkumar409@gmail.com>
2023-09-11 16:48:39 +0530

    atlas

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose")
-
-
+const mongoose = require("mongoose");
 
 const connectDatabase = function () {
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true})
-        .then(function (x) {
-            console.log("connected with db...")
-        })
-}
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(function (x) {
+      console.log("connected with db...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-
-module.exports=connectDatabase
+module.exports = connectDatabase;
